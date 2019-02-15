82d7c5b11dff2037af374b5c848cffd069946998
yon <yon.666632@gmail.com>
2019-02-15 16:18:55 +0900

    delete files and modify setup.sh

--- setup.sh
+++ setup.sh
@@ -5,47 +5,11 @@ else
     ln -s ~/dotfiles/zshrc ~/.zshrc
 fi
 
-# .vimrc
-if [ -e ~/.vimrc ]; then
-    echo "vimrc already exists."
-else
-    ln -s ~/dotfiles/vimrc ~/.vimrc
-fi
-
-# .git-completion.bash
-if [ -e ~/.git-completion.bash ]; then
-    echo "git-completion.bash already exists."
-else
-    ln -s ~/dotfiles/git-completion.bash ~/.git-completion.bash
-fi
-
-
-# .git-prompt.sh
-if [ -e ~/.git-prompt.sh ]; then
-    echo "git-prompt.sh already exists."
-else
-    ln -s ~/dotfiles/git-prompt.sh ~/.git-prompt.sh
-fi
-
 # .gitconfig
 if [ -e ~/.gitconfig ]; then
     echo "gitconfig already exists."
 else
     ln -s ~/dotfiles/gitconfig ~/.gitconfig
-fi
-
-# .tmux.conf
-if [ -e ~/.tmux.conf ]; then
-    echo "tmux.conf already exists."
-else
-    ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-fi
-
-# tool dir
-if [ -e ~/tool ]; then
-    echo "tool dir already exists."
-else
-    ln -s ~/dotfiles/tool ~/tool
 fi
 
 # .vim dir
